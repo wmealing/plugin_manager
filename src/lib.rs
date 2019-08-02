233386c0621204c7db1c39a2490037e73311961b
wmealing <wmealing@gmail.com>
2019-08-03 04:00:38 +1000

    New events.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@ pub trait Events {
     fn on_init(&self) {}
     fn on_error(&self, _err: &str) {}
     fn on_dispatch(&mut self, _s: &String) {}
-    fn on_input(&mut self, _s: &String) {}
+    fn on_input(&mut self, _s: &String) -> String {
+        "k".to_string()
+    }
+    fn on_output(&mut self, _s: &String) {}
+
     fn on_shutdown(&self) {}
     fn on_pre_read(&self) {}
     fn on_post_read(&self) {}
@@ -17,12 +21,31 @@ impl PluginManager {
         Self { hooks: Vec::new() }
     }
     pub fn add_events_hook<E: Events + 'static>(&mut self, hook: E) {
+        hook.on_init();
         self.hooks.push(Box::new(hook));
     }
 
     pub fn dispatch(&mut self, s: String) {
         for hook in &mut self.hooks {
             hook.on_dispatch(&s);
+        }
+    }
+
+    pub fn input(&mut self, s: String) -> String {
+        let mut v: Vec<String> = Vec::new();
+
+        for hook in &mut self.hooks {
+            let s = hook.on_input(&s);
+            v.push(s);
+        }
+
+        let full = v.join("");
+        full.to_string()
+    }
+
+    pub fn output(&mut self, s: String) {
+        for hook in &mut self.hooks {
+            hook.on_output(&s);
         }
     }
 }
